ecdc4fd301bbb50e1a99cd67f34a318a5a9c0462
MichaelSchuweiler <schuw012@morris.umn.edu>
2014-04-17 12:57:58 -0600

    Regexes for getting grades.

--- views/partials/unzip.js
+++ views/partials/unzip.js
@@ -1,28 +1,40 @@
 /**
  * Created by fello056 on 4/15/14.
  */
+// SUPER IMPORTANT ASSUMPTIONS AND COMPATIBILITY INFO
+// READ THE COMMENTS ABOUT THE REGULAR EXPRESSIONS USED
+// IN FUNCTIONS processEssay AND getGrades.
 
-// upload -> unzip -> make csv & fix formatting -> to API
 
-//Xml stripping
- //probably full of bugs
- //"/s/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g/
 
 function stripXml(aString) {
+    //Xml stripping
+    //probably full of bugs
+    //"/s/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g/
   return  aString.replace(foo, "")
 }
 
 function processEssay(aString) {
 //returns the whole line for csv, given an essay.
-  // fixme; the regex <fix> should match {#foo#bar#baz}
-  // as foo bar baz, separately.
   var regex = /{#.*}/
+  //This regex finds our grade segment - which has the form {#...}.
+  //It assumes that it is among the first elements of a Essay.
+  //So similar forms *CAN* be in an essay, but ours *HAS* to be first.
+  var grades = getGrades(aString.match(regex))
   var csvLine = "";
-  var grades = aString.match(regex);
+   aString.match(regex);
   for (var j = 0; j <= grades.length; j++){
       csvLine = csvLine + grades[j] + ", ";
   }
   return (csvLine + aString.replace(regex, "\r\n"))
+}
+
+function getGrades(aString){
+    var regex = /#([^#{}]*)/g
+    //Matches hash tags, globally, with any non hash character in them.
+    //So the hash ## is *NOT* ok, but all other hashes work.
+    //Even Unicode characters - #漢字 should work!
+    return aString.match(regex)
 }
 
 function unzip(zip) {
@@ -41,7 +53,7 @@ function makeCSV(zip){
     for (var i = 0; i <= fileArray.length; i++){
         //unzip the .docx
         var docx = unzip(fileArray[i])
-        processEssay(read(docx[5]))//fix the number
+        csv = csv + processEssay(read(docx[5]))//fix the number
     }
 
 }
