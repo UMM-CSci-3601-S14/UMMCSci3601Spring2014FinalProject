b3b0cd00a92d3fb8a7f9c0aed888f5776a09d603
MichaelSchuweiler <schuw012@morris.umn.edu>
2014-04-22 12:17:44 -0600

    Major changes to unzip, additions to models and scripts

--- views/partials/unzip.js
+++ views/partials/unzip.js
@@ -19,7 +19,7 @@ function processEssay(aString) {
   for (var j = 0; j <= grades.length; j++){
       csvLine = csvLine + grades[j] + ", ";
   }
-  return (csvLine + aString.replace(regex, "\r\n"))
+  return (csvLine + aString.replace(regex, "") + "\r\n")
 }
 
 function getGrades(aString){
@@ -36,11 +36,44 @@ function unzip(aBlob) {
 
 }
 
+function decompress(fullsavepath, filename){
+    zip.useWebWorkers = false;
 
+    var fileInput = document.getElementById("fileInput")
+    var file = fileInput.files[0]
+//   file_system.root.getFile("app_cache/temp.zip", {}, function(fileEntry){
+//        fileEntry.file(function (file) {
+
+            zip.createReader(new zip.BlobReader(file), function(reader) {
+
+                // get all entries from the zip
+                reader.getEntries(function(entries) {
+                    if (entries.length) {
+                        //Output zip file contents
+                        for(var i=0; i<entries.length;i++){
+                            console.log(entries[i].filename);
+                        }
+
+                        //Try to decompress all the files
+                        var fileArray = []
+                        for (var j=0;j<entries.length;j++){
+                        console.log('attempting to get ' + entries[j].filename);
+                            //the blobwriter might not be the right choice.
+                        entries[j].getData(new zip.BlobWriter(), function(unzipData){
+                            console.log(j.toString() + "'s data: " + unzipData);
+                            fileArray.push(unzipData);
+                        },null, null);
+                        return fileArray}
+                    }
+                });
+            }, null);
+
+
+}
 
 function stripXml(aString) {
     //Xml stripping
-    //probably full of bugs
+    //full of bugs
     //"/s/<[^>]\{1,\}>//g; s/[^[:print:]]\{1,\}//g/
     return  aString.replace(/foo/, "")
 }
@@ -57,11 +90,12 @@ function makeCSV(zipFile){
     var fileArray = unzip(zipFile)
     var csv =""
     if ("docx" == typeof(fileArray[0])){
-        for (var i = 0; i <= fileArray.length; i++){
-            //unzip the .docx
-            var docx = unzip(fileArray[i])
-            csv = csv + processEssay(readDocx(docx[5]))//fix the number
-        }
+        //docx not supported yet.
+//        for (var i = 0; i <= fileArray.length; i++){
+//            //unzip the .docx
+//            var docx = unzip(fileArray[i])
+//            csv = csv + processEssay(readDocx(docx[5]))//fix the number
+//        }
         return csv;
     }
     if ("txt" == typeof(fileArray[0])){
